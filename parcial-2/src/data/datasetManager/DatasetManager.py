603bec7726e0d0ddfab335ef0b54c04dcff13dc8
Matheus <matheussilva.dd@gmail.com>
2021-11-10 21:56:40 -0300

    chore: saving changes

--- parcial-2/src/data/datasetManager/DatasetManager.py
+++ parcial-2/src/data/datasetManager/DatasetManager.py
@@ -9,39 +9,89 @@ class DatasetManager:
         self.__manager = data_manager
         self.file_manager = file_manager
 
-    def setManager(self, data_manager):
+    def set_manager(self, data_manager):
         self.__manager = data_manager
 
-    def saveCountries(self):
+    def save_countries(self):
         listed_countries = self.__manager.get_index_countries()
 
-        folder_saving_path = self.file_manager.path_join(
-            self.__get_raw_data_saving_path(), "countries/countries.csv"
+        folder_saving_path = self.__get_absolute_saving_path(
+            "countries/countries.csv"
         )
 
         df = pd.DataFrame(data={'country': list(listed_countries)})
 
         df.to_csv(folder_saving_path, sep=',', index=False)
 
-    def saveIndices(self):
-        listed_indices_pd_dataframe = self.__manager.get_indices()
+    def save_indices(self):
 
-        filtered_df = self.__filter_by_major_world_indices_only(
-            listed_indices_pd_dataframe
+        world_major_indices = self.__get_major_world_indices()
+
+        indices_names_filtered_df = world_major_indices.filter(
+            items=['name', 'currency']
         )
 
-        indices_names_filtered_df = self.__get_indices_name_only(filtered_df)
-
-        folder_saving_path = self.file_manager.path_join(
-            self.__get_raw_data_saving_path(),
+        folder_saving_path = self.__get_absolute_saving_path(
             "listed-world-indices/listed-world-indices.csv"
         )
 
         indices_names_filtered_df.to_csv(
             folder_saving_path, sep=',', index=False)
 
-    def __get_indices_name_only(self, indices_df):
-        return indices_df['name']
+    def save_country_indices_relation(self):
+        major_world_indices = self.__get_major_world_indices()
+
+        country_indices_relation_table_df = (
+            self.__get_country_indices_relation_table(
+                major_world_indices
+            )
+        )
+
+        folder_saving_path = self.__get_absolute_saving_path(
+            "indices-from-country/indices-from-country.csv"
+        )
+
+        country_indices_relation_table_df.to_csv(
+            folder_saving_path, sep=',', index=False
+        )
+
+    def save_indices_historical_data(self, from_date, to_date, interval):
+        major_world_indices = self.__get_major_world_indices()
+
+        country_indices_relation_table_df = (
+            self.__get_country_indices_relation_table(major_world_indices)
+        )
+
+        for index, row in country_indices_relation_table_df.iterrows():
+            if index == 0:
+                print(self.__manager.get_index_historical_data(
+                    index=row['indice_name'],
+                    country=row['country'],
+                    from_date=from_date,
+                    to_date=to_date,
+                    interval=interval
+                ))
+
+    def __get_major_world_indices(self):
+        listed_indices_pd_dataframe = self.__manager.get_indices()
+
+        major_world_indices = self.__filter_by_major_world_indices_only(
+            listed_indices_pd_dataframe
+        )
+
+        return major_world_indices
+
+    def __get_country_indices_relation_table(self, indices_df):
+        country_indices_relation_table_df = indices_df.filter(
+            items=['country', 'name']
+        )
+
+        country_indices_relation_table_df.rename(
+            columns={'name': 'indice_name'},
+            inplace=True
+        )
+
+        return country_indices_relation_table_df
 
     def __filter_by_major_world_indices_only(self, indices_df):
         rows_selection_condition = (
@@ -55,3 +105,9 @@ class DatasetManager:
         working_directory = self.file_manager.get_working_directory()
 
         return f"{working_directory}/raw"
+
+    def __get_absolute_saving_path(self, path):
+        return self.file_manager.path_join(
+            self.__get_raw_data_saving_path(),
+            path
+        )
--- parcial-2/src/data/make_dataset.py
+++ parcial-2/src/data/make_dataset.py
@@ -11,7 +11,12 @@ from datasetManager import DatasetManager
 investAPI = InvestAPI(investpy)
 
 
-def checkIfCanWriteDataToCurrentWorkingDirectory():
+HISTORICAL_DATA_FROM_DATE = '01/01/2015'
+HISTORICAL_DATA_TO_DATE = '31/12/2020'
+HISTORICAL_DATA_INTERVAL = 'Monthly'
+
+
+def check_if_can_write_data_to_current_working_directory():
     currentWorkingDirectory = FileManager.get_working_directory()
     head, tail = FileManager.path_split(currentWorkingDirectory)
 
@@ -21,14 +26,22 @@ def checkIfCanWriteDataToCurrentWorkingDirectory():
 
 
 def main():
-    checkIfCanWriteDataToCurrentWorkingDirectory()
+    check_if_can_write_data_to_current_working_directory()
 
     indicesManager = investAPI.get_feature("indices")
     datasetManager = DatasetManager(indicesManager)
 
-    datasetManager.saveCountries()
+    datasetManager.save_countries()
 
-    datasetManager.saveIndices()
+    datasetManager.save_indices()
+
+    datasetManager.save_country_indices_relation()
+
+    datasetManager.save_indices_historical_data(
+        HISTORICAL_DATA_FROM_DATE,
+        HISTORICAL_DATA_TO_DATE,
+        HISTORICAL_DATA_INTERVAL
+    )
 
 
 if __name__ == "__main__":
